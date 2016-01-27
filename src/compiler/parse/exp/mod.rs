ab13432447e26383056a44a96d7e37f1a81cbf03
Ty Overby <ty@pre-alpha.com>
2016-01-27 09:51:42 -0800

    add closures and function calls

--- src/compiler/parse/exp/mod.rs
+++ src/compiler/parse/exp/mod.rs
@@ -10,7 +10,9 @@ pub enum Ast<'input> {
     Sub(Box<Ast<'input>>, Box<Ast<'input>>),
     Mul(Box<Ast<'input>>, Box<Ast<'input>>),
     Div(Box<Ast<'input>>, Box<Ast<'input>>),
-    Identifier(Ident<'input>)
+    Identifier(Ident<'input>),
+    FnCall(Box<Ast<'input>>, Vec<Ast<'input>>),
+    Closure(Option<Ident<'input>>, Vec<Vec<Ident<'input>>>, Vec<Ast<'input>>),
 }
 
 #[derive(Debug, PartialEq)]
@@ -60,5 +62,64 @@ mod test {
                         Box::new(Ast::IntLit(5)),
                         Box::new(Ast::IntLit(6)))),
                 Box::new(Ast::IntLit(7))));
+    }
+
+    #[test]
+    fn closures() {
+        assert!(parse_Expr("fn foo {}").is_err());
+        assert!(parse_Expr("fn").is_err());
+
+        // named
+        assert_eq!(parse_Expr("fn foo() { }").unwrap(),
+                   Ast::Closure(Some(Ident("foo")),
+                                vec![vec![]], vec![]));
+        // no name
+        assert_eq!(parse_Expr("fn() { }").unwrap(),
+                   Ast::Closure(None, vec![vec![]], vec![]));
+        // no name with params
+        assert_eq!(parse_Expr("fn(a, b) { }").unwrap(),
+                   Ast::Closure(None, vec![vec![Ident("a"), Ident("b")]], vec![]));
+
+        // named with prams
+        assert_eq!(parse_Expr("fn foo(a, b,) { }").unwrap(),
+                   Ast::Closure(Some(Ident("foo")),
+                                vec![vec![Ident("a"), Ident("b")]], vec![]));
+
+        // multi-param list
+        assert_eq!(parse_Expr("fn foo()()() { }").unwrap(),
+                   Ast::Closure(Some(Ident("foo")),
+                                vec![vec![], vec![], vec![]], vec![]));
+        assert_eq!(parse_Expr("fn foo(a, b)(c, d)(e, f) { }").unwrap(),
+                   Ast::Closure(Some(Ident("foo")),
+                                vec![vec![Ident("a"), Ident("b")],
+                                     vec![Ident("c"), Ident("d")],
+                                     vec![Ident("e"), Ident("f")]],
+                                vec![]));
+    }
+
+    #[test]
+    fn func_calling() {
+        assert_eq!(parse_Expr("foo(a, b)").unwrap(),
+            Ast::FnCall(Box::new(Ast::Identifier(Ident("foo"))),
+                        vec![Ast::Identifier(Ident("a")),
+                             Ast::Identifier(Ident("b"))]));
+
+        assert_eq!(parse_Expr("foo(a, b)(c, d)").unwrap(),
+            Ast::FnCall(Box::new(Ast::FnCall(Box::new(Ast::Identifier(Ident("foo"))),
+                                             vec![Ast::Identifier(Ident("a")),
+                                                 Ast::Identifier(Ident("b"))])),
+                        vec![Ast::Identifier(Ident("c")),
+                             Ast::Identifier(Ident("d"))]));
+
+        assert_eq!(parse_Expr("fn foo(a, b) {} (1, 2)").unwrap(),
+            Ast::FnCall(Box::new(
+                    Ast::Closure(Some(Ident("foo")),
+                                 vec![vec![Ident("a"), Ident("b")]], vec![])),
+                    vec![Ast::IntLit(1), Ast::IntLit(2)]));
+
+        assert_eq!(parse_Expr("fn(a, b) {} (1, 2)").unwrap(),
+            Ast::FnCall(Box::new(
+                    Ast::Closure(None, vec![vec![Ident("a"), Ident("b")]], vec![])),
+                    vec![Ast::IntLit(1), Ast::IntLit(2)]));
     }
 }
